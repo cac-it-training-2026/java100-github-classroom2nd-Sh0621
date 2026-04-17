33134162c9532a599ba062e713acc3b91d4b9510
hiratash <hiratash@C010423512.zipangu.cac.co.jp>
2026-04-17 16:30:15 +0900

    lesson1完了

--- Java100_questions_cac2nd/src/lesson01/challenge03/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge03/Patisserie.java
@@ -22,5 +22,19 @@
 package lesson01.challenge03;
 
 public class Patisserie {
+	public static void main(String[] args) {
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です！！");
 
+		System.out.println("\n本日のおすすめ商品です。\n");
+
+		int Cstock = 30;
+		int Sstock = 30;
+		int Pstock = 30;
+
+		System.out.println("シトロン      \\250・・・残り" + Cstock + "個");
+		System.out.println("ショコラ      \\280・・・残り" + Sstock + "個");
+		System.out.println("ピスターシュ  \\320・・・残り" + Pstock + "個");
+	}
 }
--- Java100_questions_cac2nd/src/lesson01/challenge05/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge05/Patisserie.java
@@ -29,6 +29,50 @@
 
 package lesson01.challenge05;
 
-public class Patisserie {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Patisserie {
+	public static void main(String[] args) throws IOException {
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です！！");
+
+		System.out.println("\n本日のおすすめ商品です。\n");
+
+		int Cstock = 30;
+		int Sstock = 30;
+		int Pstock = 30;
+
+		System.out.println("シトロン      \\250・・・残り" + Cstock + "個");
+		System.out.println("ショコラ      \\280・・・残り" + Sstock + "個");
+		System.out.println("ピスターシュ  \\320・・・残り" + Pstock + "個");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("\nそれぞれ何個ずつ買いますか？（最大30個まで）\n");
+		System.out.print("シトロン      >");
+		String citronBuyCountStr = br.readLine();
+		int citronBuyCount = Integer.parseInt(citronBuyCountStr);
+
+		System.out.print("ショコラ      >");
+		String chocolatBuyCountStr = br.readLine();
+		int chocolatBuyCount = Integer.parseInt(chocolatBuyCountStr);
+
+		System.out.print("ピスターシュ  >");
+		String pistacheBuyCountStr = br.readLine();
+		int pistacheBuyCount = Integer.parseInt(pistacheBuyCountStr);
+
+		System.out.println("\nシトロン     " + citronBuyCount + "個");
+		System.out.println("ショコラ     " + chocolatBuyCount + "個");
+		System.out.println("ピスターシュ " + pistacheBuyCount + "個");
+
+		int totalcount = citronBuyCount + chocolatBuyCount + pistacheBuyCount;
+		int totalprice = 250 * citronBuyCount + 280 * chocolatBuyCount + 320 * pistacheBuyCount;
+
+		System.out.println("合計個数" + totalcount + "個");
+		System.out.println("合計金額" + totalprice + "円");
+		System.out.println("\nをお買いあげですね。");
+		System.out.println("承りました。");
+	}
 }
